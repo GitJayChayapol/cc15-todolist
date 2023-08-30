99ef366f9e9002421bb2580f632ca97973ed0bf1
GitJayChayapol <vongjitpimol.jay@hotmail.com>
2023-08-30 10:01:16 +0700

    update new file Sidebar

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,20 +1,14 @@
 import "./App.scss";
+import "../components/Header.scss";
+
+import Header from "../components/Header";
 import { FaHome } from "react-icons/fa";
 function App() {
   return (
     <div className="todo">
       <div className="todo__header">
-        <header className="header">
-          <div className="header__logo">
-            <FaHome />
-          </div>
-          <div className="header__text">
-            <h1>Todoist</h1>
-          </div>
-          <div className="header__search">
-            <input></input>
-          </div>
-        </header>
+        {" "}
+        <Header />
       </div>
       <div className="todo__sidebar">SideBar</div>
       <div className="todo__content">TodoContent</div>
