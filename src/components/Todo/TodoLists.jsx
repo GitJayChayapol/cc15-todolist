1323c76e69201c64a465228fdd30d7973729dc58
GitJayChayapol <vongjitpimol.jay@hotmail.com>
2023-08-30 17:18:16 +0700

    todoItem

--- src/components/Todo/TodoLists.jsx
+++ src/components/Todo/TodoLists.jsx
@@ -1,9 +1,26 @@
-import styles from './TodoHeader.module.scss'
+import styles from './TodoLists.module.scss';
+// import { FaTrashAlt,Fapen } from 'react-icons/fa';
+// import { FaHome } from "react-icons/fa";
+import { FaTrashAlt,FaPen } from 'react-icons/fa';
 
 function TodoLists() {
   return (
-    <div>TodoLists</div>
-  )
+    <ul className={styles.todo__list}>
+      <li className={styles.todo}>
+        <span className={styles.todo__checkbox}></span>
+        <p className={styles.todo__task}>TodoItem 1</p>
+        <span className={styles.todo__date}>30 AUG</span>
+        <div className={styles.todo__action}>
+          <span className={styles.todo__edit}>
+            <FaPen/>
+          </span>
+          <span className={styles.todo__delete}>
+            <FaTrashAlt/>
+          </span>
+        </div>
+      </li>
+    </ul>
+  );
 }
 
 export default TodoLists;
