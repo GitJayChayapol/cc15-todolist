c34b7abfa75fe6bc86cb2d6af6693d3d7dee545e
GitJayChayapol <vongjitpimol.jay@hotmail.com>
2023-08-30 12:13:02 +0700

    refactor:create LisItem component

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,16 +1,27 @@
 import "./App.scss";
 import "../components/Header.scss";
-
+import Listitem from "../components/Listitem";
 import Header from "../components/Header";
-import { FaHome } from "react-icons/fa";
+import { FaInbox, FaCalendar, FaCalendarAlt, FaChevronDown } from 'react-icons/fa';
 function App() {
   return (
     <div className="todo">
       <div className="todo__header">
-        {" "}
         <Header />
       </div>
-      <div className="todo__sidebar">SideBar</div>
+      <div className="todo__sidebar">
+        <aside className="sidebar">
+          <section className="sidebar__category">
+            <ul className="list">
+              <Listitem text="Inbox" icon={<FaInbox className="list__item__icon"/>}/>
+              <Listitem text="Today" icon={<FaCalendar className="list__item__icon"/>}/>
+              <Listitem text="Next 7 day" icon={<FaCalendarAlt className="list__item__icon"/>}/>
+            </ul>
+          </section>
+          <section className="sidebar__category">2</section>
+
+        </aside>
+      </div>
       <div className="todo__content">TodoContent</div>
     </div>
   );
---
+++ src/components/Listitem.jsx
@@ -0,0 +1,13 @@
+import './Listitem.scss'
+import { FaInbox } from 'react-icons/fa';
+
+function Listitem(props){
+    console.log(props)
+    return (
+        <li className="list__item">
+                  {props.icon}
+            <p className="list__item__text">{props.text}</p>
+        </li>
+    )
+}
+export default Listitem;
