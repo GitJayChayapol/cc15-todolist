71d935eb43ab7cd0517f0e5735be40bdb5cea6b9
GitJayChayapol <vongjitpimol.jay@hotmail.com>
2023-08-31 11:45:27 +0700

    UI : replace button in form with Button Component

--- src/components/Todo/TodoLists.jsx
+++ src/components/Todo/TodoLists.jsx
@@ -1,25 +1,32 @@
+import { useState } from 'react';
 import styles from './TodoLists.module.scss';
 import { FaTrashAlt, FaPen } from 'react-icons/fa';
 import { HiOutlineCheck } from 'react-icons/hi';
+import TodoForm from './TodoForm';
 
 function TodoLists() {
+  const [listOpenForm,setlistOpenForm] = useState(false)
+  const listClick = function (e) {
+    setlistOpenForm(!listOpenForm)
+  }
   return (
     <ul className={styles.todo__lists}>
-      <li className={styles.todo}>
-        <div className={styles.todo__checkbox}>
+    {listOpenForm? (<TodoForm/>) : 
+      (<li className={styles.todo}>
+        <div className={`${styles.todo__checkbox} ${styles.todo__checkbox__done}`}>
           <HiOutlineCheck className={styles.todo__checkbox__icon} />
         </div>
-        <p className={styles.todo__task}>todo-item 1 </p>
+        <p className={`${styles.todo__task} ${styles.todo__task__done}`}>todo-item 1 </p>
         <span className={styles.todo__date}>30 Aug</span>
         <div className={styles.todo__action}>
           <span>
-            <FaPen className={styles.todo__edit} />
+            <FaPen className={styles.todo__edit} onClick = {listClick} />
           </span>
           <span>
             <FaTrashAlt className={styles.todo__delete} />
           </span>
         </div>
-      </li>
+      </li>)}
     </ul>
   );
 }
