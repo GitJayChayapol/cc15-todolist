5407c2a75f8e13aa01acf7dd34b9b2348660e438
GitJayChayapol <vongjitpimol.jay@hotmail.com>
2023-08-31 09:25:09 +0700

    PV Project

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,48 +1,26 @@
-import "./App.scss";
-import Listitem from "../components/Listitem";
-import Header from "../components/Header";
-import Lists from "../components/List";
-import TodoHeader from "../components/Todo/TodoHeader";
-import TodoCreate from "../components/Todo/TodoCreate";
-import TodoLists from "../components/Todo/TodoLists";
-import { FaInbox, FaCalendar, FaCalendarAlt, FaChevronDown } from 'react-icons/fa';
-function App() {
-  const genealLists = [
-    {id:1,text:'Inbox',active:true,icon:<FaInbox/>},
-    {id:2,text:'Today',active:false,icon:<FaCalendar/>},
-    {id:3,text:'Next 7 day',active:false,icon:<FaCalendarAlt/>},]
-  const projectLists = [
-    {id:1,text:'Project-A',icon:<FaInbox/>, active:true},
-    {id:2,text:'Project-B',icon:<FaInbox/>, active:false}]
-  return (
-    <div className="todo">
-      <div className="todo__header">
-        <Header />
-      </div>
-      <div className="todo__sidebar">
-        <aside className="sidebar">
-          <section className="sidebar__category">
-            <Lists data={genealLists}/>
-          </section>
-          <section className="sidebar__category">
-            <div className="accordion">
-              <div className="accordion__toggle">
-                <li className="accordion__item">
-                  <FaChevronDown className="accordion__item__icon accordion__item__active"/>
-                  <p className="accordion__item__text">Projects</p>
-                </li>
-              </div>
-              <Lists data={projectLists}/>
-            </div>
-          </section>
+// Dependencies
+import './App.scss';
+import AppBar from '../components/Common/AppBar/AppBar';
+import SideBar from '../components/SideBar/SideBar';
+import TodoHeader from '../components/Todo/TodoHeader';
+import TodoCreate from '../components/Todo/TodoCreate';
+import TodoLists from '../components/Todo/TodoLists';
+import { Button } from '../components/Common/Button/Button';
 
-        </aside>
+function App() {
+  return (
+    <div className='todo'>
+      <div className='todo__header'>
+        <AppBar />
       </div>
-      <div className="todo__content">
-        <main className="todo__container">
-          <TodoHeader/>
-          <TodoCreate/>
-          <TodoLists/>
+      <div className='todo__sidebar'>
+        <SideBar />
+      </div>
+      <div className='todo__content'>
+        <main className='todo__container'>
+          <TodoHeader />
+          <TodoCreate />
+          <TodoLists />
         </main>
       </div>
     </div>
@@ -50,11 +28,3 @@ function App() {
 }
 
 export default App;
-
-/* Challenge
-- ให้ 2 section render UI ที่...
-  - Option A (2/5): render UI ต่างกัน <List/> กับ <Accordian/> : Difficult 2.5
-  - Option B (4/5): render UI เดียวกัน เช่น <Lists/>
-  - Option C (5/5): render UI <Lists/> ภายใต้ <Accordian> <Lists/> <Accordian>
-  // ใช้ props.children
-*/
